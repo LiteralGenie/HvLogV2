8be6df71a04dbee8f6e3c4f957ef023d1d568ab5
Literal Genie <frothsavd@gmail.com>
2022-08-12 20:05:14 -0400

    Break parser logic into smaller functions

--- run_server.py
+++ run_server.py
@@ -1,15 +1,22 @@
+from typing import TypedDict
+
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
-from classes.models import Battle, BattleReport, Event, Turn
+from classes.models import ActiveBattleTurn, Battle, BattleReport
 from config import paths
 from utils.parse import parse_events
 
 
 class RawTurnLog(BaseModel):
     lines: list[str]
+    time: int | None
+
+
+class Turn(TypedDict):
+    events: list[dict]
     time: int | None
 
 
@@ -23,204 +30,126 @@ app.add_middleware(
 )
 
 
+def get_active_battle(
+    round_start_event: dict | None = None,
+) -> tuple[Battle, BattleReport | None, bool]:
+    is_new_battle = False
+
+    # Get or create latest battle
+    active_battle = Battle.get_or_none(Battle.active == True)
+    if active_battle is None:
+        active_battle = Battle.create(active=True)
+
+    # Get latest battle info
+    active_report = (
+        BattleReport.select().join(Battle).where(Battle.pk == active_battle.pk).first()
+    )
+
+    # If we have info to create or update a report...
+    if ev := round_start_event:
+        if active_report is None:
+            # If no existing report, create one
+            active_report = BattleReport.create(
+                type="meta",
+                data=dict(
+                    battle_type=ev["battle_type"],
+                    last_round=ev["current"],
+                    max_rounds=ev["max"],
+                ),
+                battle=active_battle,
+            )
+        else:
+            # Else check if event is for a new battle
+            is_earlier_round = ev["current"] < active_report.data["last_round"]
+            is_different_type = ev["battle_type"] != active_report.data["battle_type"]
+            is_different_round_max = ev["max"] != active_report.data["max_rounds"]
+
+            if is_different_type or is_different_round_max or is_earlier_round:
+                # Event is for new battle, so create battle / report
+                active_battle.update(active=False).execute()
+                active_battle = Battle.create(active=True)
+                is_new_battle = True
+
+                active_report = BattleReport.create(
+                    type="meta",
+                    data=dict(
+                        battle_type=ev["battle_type"],
+                        last_round=ev["current"],
+                        max_rounds=ev["max"],
+                    ),
+                    battle=active_battle,
+                )
+            else:
+                # Event is for current battle so update battle report
+                active_report.data["last_round"] = ev["current"]
+                active_report.save()
+
+    return active_battle, active_report, is_new_battle
+
+
+def parse_logs(raw_logs: list[RawTurnLog]) -> tuple[list[Turn], list[str]]:
+    # Sort by time
+    raw_sorted = [
+        log
+        for idx, log in sorted(
+            enumerate(raw_logs), key=lambda pair: pair[1].time or pair[0]
+        )
+    ]
+
+    # Parse into events
+    events = parse_events([line for turn in raw_sorted for line in turn.lines])
+
+    # Regroup into Turns
+    rejects: list[str] = []
+    turns: list[Turn] = []
+    for raw_log in raw_logs:
+        turns.append(Turn(events=[], time=raw_log.time))
+        for line in raw_log.lines:
+            ev = events.pop(0)
+            if ev:
+                turns[-1]["events"].append(ev)
+            else:
+                rejects.append(line)
+
+    return turns, rejects
+
+
 @app.post("/logs")
-def post_logs(turn_logs: list[RawTurnLog]):
+def post_logs(raw_logs: list[RawTurnLog]):
     """
     Assumptions:
         -> All turn_logs are for the same battle
         -> At least one log starts with a ROUND_START event
     """
 
-    # Sort by time
-    turn_logs = [
-        log
-        for idx, log in sorted(
-            enumerate(turn_logs), key=lambda pair: pair[1].time or pair[0]
-        )
-    ]
+    # Parse logs
+    [turns, unparseable] = parse_logs(raw_logs)
+    for line in unparseable:
+        print(f"WARNING: Failed to parse line [{line}]")
 
-    # Parse into events
-    turn_inds = [i for i, turn in enumerate(turn_logs) for _ in turn.lines]
-    lines = [line for turn in turn_logs for line in turn.lines]
-    events = parse_events(lines)
-
+    # Get or create active battle
     round_start = next(
-        (ev for ev in events if ev and ev["event_type"] == "ROUND_START"), None
+        (
+            ev
+            for turn in turns
+            for ev in turn["events"]
+            if ev and ev["event_type"] == "ROUND_START"
+        ),
+        None,
     )
-
-    # Get or create latest battle
-    active_battle = Battle.get_or_none(Battle.active == True)
-
-    if active_battle is None:
-        active_battle = Battle.create(active=True)
-
-    # Get latest battle info
-    active_battle_report = (
-        BattleReport.select().join(Battle).where(Battle.id == active_battle.id).first()
-    )
-
-    # Check if logs are for new battle
-    if round_start:
-
-        def is_earlier_round(current: int):
-            ev: Event = (
-                Event.select()
-                .join(Turn)
-                .join(Battle)
-                .where(Event.type == "ROUND_START", Battle.id == active_battle.id)
-                .order_by(Event.idx.desc())
-                .first()
-            )
-            last_round: int = ev.parsed_data["current"] if ev else float("inf")
-
-            return current < last_round
-
-        is_different_type = (
-            active_battle_report
-            and round_start["battle_type"] != active_battle_report.type
-        )
-        is_different_round_max = (
-            active_battle_report
-            and round_start["max"] != active_battle_report.data["rounds"]
-        )
-
-        if (
-            is_different_type
-            or is_different_round_max
-            or is_earlier_round(round_start["current"])
-        ):
-            active_battle.update(active=False).execute()
-            active_battle = Battle.create(active=True)
-
-            active_battle_report = BattleReport.create(
-                type="meta",
-                data=dict(
-                    battle_type=round_start["battle_type"],
-                    rounds=round_start["max"],
-                ),
-                battle=active_battle,
-            )
-        elif active_battle_report is None:
-            active_battle_report = BattleReport.create(
-                type="meta",
-                data=dict(
-                    battle_type=round_start["event_type"], rounds=round_start["max"]
-                ),
-                battle=active_battle,
-            )
-
-    # Get largest turn index
-    turn_idx_start = (
-        Turn.select()
-        .join(Battle)
-        .where(Battle.id == active_battle.id)
-        .order_by(Turn.idx.desc())
-        .first()
-    )
-    turn_idx_start = turn_idx_start.idx if turn_idx_start else -1
-
-    # Get largest event index
-    last_ev_idx = (
-        Event.select()
-        .join(Turn)
-        .join(Battle)
-        .where(Battle.id == active_battle.id)
-        .order_by(Event.idx.desc())
-        .first()
-    )
-    last_ev_idx = last_ev_idx.idx if last_ev_idx else -1
+    active_battle, active_report, is_new_battle = get_active_battle(round_start)
 
     # Append events to db
-    current_turn = None
-    current_turn_idx = None
-    for i, event, line in zip(turn_inds, events, lines):
-        turn_idx = turn_idx_start + i + 1
-
-        if event is None:
-            print(f"WARNING: Failed to parse line [{line}]")
-            continue
-
-        # Get turn to append to
-        if current_turn_idx != turn_idx:
-            current_turn = Turn.create(
-                idx=turn_idx, time=turn_logs[i].time, battle=active_battle
-            )
-            current_turn_idx = turn_idx
-
-        # Compress data
-        for k in event.keys():
-            if k not in active_battle._key_map:
-                active_battle._key_map.append(k)
-                active_battle.save()
-        event_data = {active_battle._key_map.index(k): v for k, v in event.items()}
-
-        # Append
-        ev = Event.create(
-            idx=last_ev_idx + 1,
-            type=event["event_type"],
-            data=event_data,
-            turn=current_turn,
-            battle=active_battle,
+    for turn in turns:
+        ActiveBattleTurn.create(
+            events=turn["events"], meta=dict(time=turn["time"]), battle=active_battle
         )
-        last_ev_idx += 1
 
     # Append events to file
-    out_file = paths.BATTLE_LOG_DIR / f"{active_battle.id}.hv"
+    out_file = paths.BATTLE_LOG_DIR / f"{active_battle.pk}.hv"
     with open(out_file, "a") as file:
-        turns = [t.json() for t in turn_logs]
+        turns = [t.json() for t in raw_logs]
         file.write("\n".join(turns) + "\n")
-
-    # Receive raw log
-    # Parse into event
-    # If...
-    #   No prev log -> create log
-    #   Is new log  -> create log
-    #   Else        -> noop
-    # Append into db
-    # Append into file
-    # Update report
-
-    # Storage format
-    # HEADER {
-    #   id: ...
-    #   created: ...
-    #   meta: ... health, level, stuff when battle started but not contained in logs
-    # }
-    # TURN {
-    #   time: ... optional
-    #   lines: [...]
-    #   meta: ... battle state?
-    # }
-
-    # DB schema
-    #
-    # Battle:
-    #   id              created         type        rounds      active      meta
-    #   INTEGER         REAL            TEXT        INTEGER     BOOL        JSON
-    #   PRIMARY KEY     NOT NULL        NOT NULL    NOT NULL    NOT NULL
-    #
-    # Turn:
-    #   id              battle_id       meta        time
-    #   INTEGER         INTEGER         JSON        REAL
-    #   PRIMARY KEY     FOREIGN KEY
-    #                   PRIMARY KEY
-    #
-    # Event:
-    #   id             turn_id         battle_id       type         data
-    #   INTEGER        INTEGER         INTEGER         TEXT         JSON
-    #   PRIMARY KEY    PRIMARY KEY     PRIMARY KEY     NOT NULL     NOT NULL
-    #                                  FOREIGN KEY
-    # Report:
-    #   battle_id       type            data
-    #   INTEGER         TEXT            JSON
-    #   PRIMARY KEY     PRIMARY KEY     NOT NULL
-
-    # Report generator
-    # How to allow custom user parser?
-    #   JS allowed?
-    # Where do generators go?
-
-    # Cases
 
 
 class ReportGenerator:
